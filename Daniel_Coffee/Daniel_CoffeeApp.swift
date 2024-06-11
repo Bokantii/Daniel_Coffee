54b759b342f7f2f852434520be74ea15f3ca51d0
Daniel Bokantero <teejaysoarz@gmail.com>
2024-06-11 15:51:59 -0400

    Initial Commit

---
+++ Daniel_Coffee/Daniel_CoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daniel_CoffeeApp.swift
+//  Daniel_Coffee
+//
+//  Created by User on 2024-06-11.
+//
+
+import SwiftUI
+
+@main
+struct Daniel_CoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
